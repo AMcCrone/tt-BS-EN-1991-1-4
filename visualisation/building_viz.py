448ea3790684e4284ec58c2efa515635f902f97b
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-04-16 15:29:39 +0100

    Update building_viz.py

--- visualisation/building_viz.py
+++ visualisation/building_viz.py
@@ -11,27 +11,27 @@ def create_building_visualisation(NS_dimension, EW_dimension, z):
     
     # Add the base
     fig.add_trace(go.Scatter3d(x=x, y=y, z=z_base, mode='lines', line=dict(color='blue', width=3),
-                              name='Base'))
+                              showlegend=False))
     
     # Add the top
     fig.add_trace(go.Scatter3d(x=x, y=y, z=z_top, mode='lines', line=dict(color='blue', width=3),
-                              name='Top'))
+                              showlegend=False))
     
     # Add the vertical edges
     for i in range(4):
         fig.add_trace(go.Scatter3d(x=[x[i], x[i]], y=[y[i], y[i]], z=[0, z], mode='lines',
                                   line=dict(color='blue', width=3), showlegend=False))
     
-    # Set the layout to be minimal but clear
+    # Set the layout to be minimal with no axes or grid
     fig.update_layout(
         scene=dict(
-            xaxis=dict(title='North-South (m)', showbackground=False, showgrid=True, zeroline=True),
-            yaxis=dict(title='East-West (m)', showbackground=False, showgrid=True, zeroline=True),
-            zaxis=dict(title='Height (m)', showbackground=False, showgrid=True, zeroline=True),
+            xaxis=dict(visible=False, showgrid=False, showticklabels=False, showbackground=False, zeroline=False),
+            yaxis=dict(visible=False, showgrid=False, showticklabels=False, showbackground=False, zeroline=False),
+            zaxis=dict(visible=False, showgrid=False, showticklabels=False, showbackground=False, zeroline=False),
             aspectmode='data'
         ),
-        margin=dict(l=0, r=0, b=0, t=30),
-        legend=dict(x=0, y=0),
+        margin=dict(l=0, r=0, b=0, t=0),
+        showlegend=False,
         scene_camera=dict(
             eye=dict(x=1.5, y=-1.5, z=1.2)
         ),
