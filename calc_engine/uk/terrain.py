89ace81108fef430036ad9b99b149656a7176067
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-04-16 09:24:26 +0100

    Update terrain.py

--- calc_engine/uk/terrain.py
+++ calc_engine/uk/terrain.py
@@ -1,1 +1,10 @@
+def get_terrain_categories():
+    """
+    Returns terrain categories according to UK National Annex
+    """
+    return [
+        "Sea - Open sea, lakes and coastal areas exposed to open sea",
+        "Country - Flat open country without obstacles",
+        "Town - Town or city terrain with closely spaced obstacles"
+    ]
 
