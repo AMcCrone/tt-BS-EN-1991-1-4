3f0975b346955493000f39bf8ab6622b1e2b4a1d
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-04-16 15:34:29 +0100

    Update building_viz.py

--- visualisation/building_viz.py
+++ visualisation/building_viz.py
@@ -1,26 +1,92 @@
 import plotly.graph_objects as go
 def create_building_visualisation(NS_dimension, EW_dimension, z):
-    # Create a simple 3D building model
-    x = [0, NS_dimension, NS_dimension, 0, 0]
-    y = [0, 0, EW_dimension, EW_dimension, 0]
-    z_base = [0, 0, 0, 0, 0]
-    z_top = [z, z, z, z, z]
+    # Define colors
+    TT_LightBlue = "rgb(136,219,223)"
+    TT_MidBlue = "rgb(0,163,173)"
     
-    # Create a blank figure with minimal styling
+    # Create a blank figure
     fig = go.Figure()
     
-    # Add the base
-    fig.add_trace(go.Scatter3d(x=x, y=y, z=z_base, mode='lines', line=dict(color='blue', width=3),
-                              showlegend=False))
+    # Define vertices for each face of the building
+    # North-South faces (mid blue)
+    # Front face (y=0)
+    fig.add_trace(go.Mesh3d(
+        x=[0, NS_dimension, NS_dimension, 0],
+        y=[0, 0, 0, 0],
+        z=[0, 0, z, z],
+        i=[0, 0],
+        j=[1, 2],
+        k=[2, 3],
+        color=TT_MidBlue,
+        opacity=1,
+        showlegend=False
+    ))
     
-    # Add the top
-    fig.add_trace(go.Scatter3d(x=x, y=y, z=z_top, mode='lines', line=dict(color='blue', width=3),
-                              showlegend=False))
+    # Back face (y=EW_dimension)
+    fig.add_trace(go.Mesh3d(
+        x=[0, NS_dimension, NS_dimension, 0],
+        y=[EW_dimension, EW_dimension, EW_dimension, EW_dimension],
+        z=[0, 0, z, z],
+        i=[0, 0],
+        j=[1, 2],
+        k=[2, 3],
+        color=TT_MidBlue,
+        opacity=1,
+        showlegend=False
+    ))
     
-    # Add the vertical edges
-    for i in range(4):
-        fig.add_trace(go.Scatter3d(x=[x[i], x[i]], y=[y[i], y[i]], z=[0, z], mode='lines',
-                                  line=dict(color='blue', width=3), showlegend=False))
+    # East-West faces (light blue)
+    # Left face (x=0)
+    fig.add_trace(go.Mesh3d(
+        x=[0, 0, 0, 0],
+        y=[0, EW_dimension, EW_dimension, 0],
+        z=[0, 0, z, z],
+        i=[0, 0],
+        j=[1, 2],
+        k=[2, 3],
+        color=TT_LightBlue,
+        opacity=1,
+        showlegend=False
+    ))
+    
+    # Right face (x=NS_dimension)
+    fig.add_trace(go.Mesh3d(
+        x=[NS_dimension, NS_dimension, NS_dimension, NS_dimension],
+        y=[0, EW_dimension, EW_dimension, 0],
+        z=[0, 0, z, z],
+        i=[0, 0],
+        j=[1, 2],
+        k=[2, 3],
+        color=TT_LightBlue,
+        opacity=1,
+        showlegend=False
+    ))
+    
+    # Top face (z=z)
+    fig.add_trace(go.Mesh3d(
+        x=[0, NS_dimension, NS_dimension, 0],
+        y=[0, 0, EW_dimension, EW_dimension],
+        z=[z, z, z, z],
+        i=[0, 0],
+        j=[1, 2],
+        k=[2, 3],
+        color=TT_LightBlue,
+        opacity=1,
+        showlegend=False
+    ))
+    
+    # Bottom face (z=0)
+    fig.add_trace(go.Mesh3d(
+        x=[0, NS_dimension, NS_dimension, 0],
+        y=[0, 0, EW_dimension, EW_dimension],
+        z=[0, 0, 0, 0],
+        i=[0, 0],
+        j=[1, 2],
+        k=[2, 3],
+        color=TT_LightBlue,
+        opacity=1,
+        showlegend=False
+    ))
     
     # Set the layout to be minimal with no axes or grid
     fig.update_layout(
