7605fd3842fdd8e4554a651812df3a781ad94ef2
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-04-23 21:54:28 +0100

    Update util.py

--- calc_engine/common/util.py
+++ calc_engine/common/util.py
@@ -1,1 +1,22 @@
+def get_session_value(st, key, default_value=None):
+    """Get a value from session state with a default fallback.
+    
+    Args:
+        st: Streamlit object
+        key: The session state key
+        default_value: Default value if key doesn't exist
+        
+    Returns:
+        The value from session state or the default
+    """
+    return st.session_state.inputs.get(key, default_value)
 
+def store_session_value(st, key, value):
+    """Store a value in session state.
+    
+    Args:
+        st: Streamlit object
+        key: The session state key
+        value: The value to store
+    """
+    st.session_state.inputs[key] = value
