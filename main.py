088ed7addda0d17f0d96caadcd04e93240106d04
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-05-22 17:01:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from auth import authenticate_user
 from calc_engine.uk.terrain import get_terrain_categories as get_uk_terrain
 from calc_engine.eu.terrain import get_terrain_categories as get_eu_terrain
 from visualisation.building_viz import create_building_visualisation
-from visualisation.map import render_map, get_elevation, compute_distance
+from visualisation.map import render_map_with_markers, get_elevation, compute_distance
 from educational import text_content
 
 authenticate_user()
