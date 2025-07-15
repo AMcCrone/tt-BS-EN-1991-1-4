97eb204993d37e524ae3de5011be54d4c9b5ebab
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-07-15 22:12:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ st.image("educational/images/TT_Logo_Colour.png", width=450, output_format="PNG"
 
 # Simple title and subtitle using Streamlit's built-in functions
 st.title("Wind Load Calculator")
-st.caption("Wind Load Calculation to BS EN 1991-1-4 and UK National Annex — So simple it'll blow you away... 🥁💨")
+st.caption("Wind Load Calculation to BS EN 1991-1-4 and UK National Annex")
 
 TT_Grey = "rgb(99,102,105)"
 st.markdown("""
