f87efaf8a6d987c713032f81d137e528c85ba801
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-09-10 18:23:16 +0100

    Update directional_viz.py

--- visualisation/directional_viz.py
+++ visualisation/directional_viz.py
@@ -254,10 +254,6 @@ def create_cdir_radial_plot(height=400, width=400, rotation_angle=0, NS_dimensio
 
         # Keep text vertical irrespective of rotation. Set to False to keep horizontal instead.
         vertical_labels = True
-        if vertical_labels:
-            fixed_text_angle = 0  # vertical text (rotate 90° CCW)
-        else:
-            fixed_text_angle = 0   # horizontal text
 
         for dir_label, info in face_local.items():
             cx_local, cy_local = info["centre"]
