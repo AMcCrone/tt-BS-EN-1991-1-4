50a1699def4c8becfb9fe13cc60cd09a6197f892
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-11-17 20:57:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ show_educational = st.sidebar.checkbox(
     value=st.session_state.get("show_educational", False)
 )
 
-col1, col2, col3 = st.columns([1, 4.4, 1])
+col1, col2, col3 = st.columns([1, 4.5, 1])
 with col2:
     st.image("educational/images/TT_Logo_Colour.svg", width=450)
     st.title("Wind Load Calculator")
