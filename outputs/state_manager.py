8eb82d6db98e869fbc4def4e820d0c2326e74748
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-11-26 22:23:57 +0000

    Update state_manager.py

--- outputs/state_manager.py
+++ outputs/state_manager.py
@@ -270,7 +270,7 @@ class StateManager:
             "z_minus_h_dis": get_result("z_minus_h_dis"),
             
             # Wind velocities and pressures
-            "v_b": get_result("v_b"),
+            "V_b": get_result("v_b"),
             "v_mean": get_result("v_mean"),
             "q_b": get_result("q_b"),
             "qp_value": get_result("qp_value"),
